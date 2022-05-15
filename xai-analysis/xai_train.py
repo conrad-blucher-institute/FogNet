9ca6a042a0a38a1c7dd3b943c2b3fcf789800a6b
hkaman7 <hamid.kamangir@gmail.com>
2022-05-14 22:46:04 -0500

    5 iteration xai groupwise results

--- xai-analysis/xai_train.py
+++ xai-analysis/xai_train.py
@@ -18,7 +18,7 @@ from numpy import savez_compressed
 from optparse import OptionParser
 from scipy.interpolate import (UnivariateSpline, RectBivariateSpline, RegularGridInterpolator)
 from sklearn.metrics import confusion_matrix
-
+import statistics
 import tensorflow
 from tensorflow import keras
 from tensorflow.keras.utils import to_categorical
@@ -56,9 +56,7 @@ from sklearn.metrics import confusion_matrix, accuracy_score, classification_rep
 #import FogNetConfig
 import xai_engine
 
-import sys
-sys.path.append('../')
-from src import utils, FogNet, FogNetConfig, applyThreshold, calcMetrics, cnn_evaluate
+import utils, FogNet, FogNetConfig, cnn_evaluate
 
 
 DEFAULT_IMAGE_DIR_NAME = ('/data1/fog-data/fog-maps/')
@@ -125,82 +123,84 @@ mur_file_names = [mur_file_template.format(year=year) for year in allYears]
 targets_file_template = "target{year}_{horizon}.csv"
 targets_file_names = [targets_file_template.format(year=year, horizon=horizons[2]) for year in allYears]
 
-with strategy.scope():
+
     # Read data cubes
-    training_list   = utils.load_Cat_cube_data(nam_G1_names,
-        nam_G2_names, nam_G3_names, nam_G4_names, mixed_file_names, mur_file_names, DEFAULT_CUBES_24_DIR_NAME, trainYearIdxs)
+training_list   = utils.load_Cat_cube_data(nam_G1_names,
+    nam_G2_names, nam_G3_names, nam_G4_names, mixed_file_names, mur_file_names, DEFAULT_CUBES_24_DIR_NAME, trainYearIdxs)
 
-    validation_list = utils.load_Cat_cube_data(nam_G1_names,
-        nam_G2_names, nam_G3_names, nam_G4_names, mixed_file_names, mur_file_names, DEFAULT_CUBES_24_DIR_NAME, valYearIdxs)
+validation_list = utils.load_Cat_cube_data(nam_G1_names,
+    nam_G2_names, nam_G3_names, nam_G4_names, mixed_file_names, mur_file_names, DEFAULT_CUBES_24_DIR_NAME, valYearIdxs)
 
-    testing_list    = utils.load_Cat_cube_data(nam_G1_names,
-        nam_G2_names, nam_G3_names, nam_G4_names, mixed_file_names, mur_file_names, DEFAULT_CUBES_24_DIR_NAME, testYearIdxs)
+testing_list    = utils.load_Cat_cube_data(nam_G1_names,
+    nam_G2_names, nam_G3_names, nam_G4_names, mixed_file_names, mur_file_names, DEFAULT_CUBES_24_DIR_NAME, testYearIdxs)
 
-    target_class = utils.targets(
-        targets_file_names, trainYearIdxs, valYearIdxs, testYearIdxs,
-        DEFAULT_TARGET_24_DIR_NAME,
-        0, # priority_calss: the last integer value is the class of target to predict: 0: is < 1600; 1: < 3200 and 2: < 6400
-    )
-    target_list = target_class.binary_target()
+target_class = utils.targets(
+    targets_file_names, trainYearIdxs, valYearIdxs, testYearIdxs,
+    DEFAULT_TARGET_24_DIR_NAME,
+    0, # priority_calss: the last integer value is the class of target to predict: 0: is < 1600; 1: < 3200 and 2: < 6400
+)
+target_list = target_class.binary_target()
 
-    # Separate into train, test, validation
-    Training_targets = target_list[0]
-    print('training target shape:', Training_targets.shape)
-    ytrain = target_list[1]
-    print('training categorical target shape:', ytrain.shape)
-    Validation_targets = target_list[2]
-    print('validation target shape:', Validation_targets.shape)
-    yvalid = target_list[3]
-    print('validation categorical target shape:', yvalid.shape)
-    Testing_targets = target_list[4]
-    print('testing target shape:', Testing_targets.shape)
-    ytest = target_list[5]
-    print('testing categorical target shape:', ytest.shape)
+# Separate into train, test, validation
+Training_targets = target_list[0]
+print('training target shape:', Training_targets.shape)
+ytrain = target_list[1]
+print('training categorical target shape:', ytrain.shape)
+Validation_targets = target_list[2]
+print('validation target shape:', Validation_targets.shape)
+yvalid = target_list[3]
+print('validation categorical target shape:', yvalid.shape)
+Testing_targets = target_list[4]
+print('testing target shape:', Testing_targets.shape)
+ytest = target_list[5]
+print('testing categorical target shape:', ytest.shape)
 
-
+#with strategy.scope():
     # Initialize
-    learningRate = 0.0009 # hyperparameters[key][0] 
-    wd           = 0.001  # hyperparameters[key][1] 
-    filters      = 24     # hyperparameters[key][2] 
-    dropout      = 0.3    # hyperparameters[key][3] 
+learningRate = 0.0009 # hyperparameters[key][0] 
+wd           = 0.001  # hyperparameters[key][1] 
+filters      = 24     # hyperparameters[key][2] 
+dropout      = 0.3    # hyperparameters[key][3] 
 
 
-    cnn_file_name = '/data1/fog/fognn/FogNet/trained_model/single_gpu_weights.h5'
-    #cnn_file_name = '/data1/fog/fognn/FogNet/trained_model/weights.h5'
-    input_nam_shape       = Input((32, 32, 288, 1))
-    input_mur_shape       = Input((384, 384, 1, 1)) 
-    input_nam_G1_24_shape = Input((32, 32, 108, 1))
-    input_nam_G2_24_shape = Input((32, 32, 96, 1))
-    input_nam_G3_24_shape = Input((32, 32, 108, 1))
-    input_nam_G4_24_shape = Input((32, 32, 60, 1))
-    input_mixed_24_shape  = Input((32, 32, 12, 1))
-    
-    
-    C  = FogNet.FogNet(input_nam_G1_24_shape, 
-                   input_nam_G2_24_shape, 
-                   input_nam_G3_24_shape, 
-                   input_nam_G4_24_shape, 
-                   input_mixed_24_shape, 
-                   input_mur_shape,
-                   filters, dropout,
-        filters, dropout, 2)
-    cnn_model_object = C.BuildModel()
-    #cnn_model_object = multi_gpu_model(cnn_model_object, gpus=4)
-    #model.summary() 
-
-    cnn_model_object.load_weights(cnn_file_name)  
-
-    cnn_model_object.compile(optimizer=Adam(lr=learningRate, decay=wd),
-          loss='categorical_crossentropy',
-          metrics=['accuracy'])
+cnn_file_name = '/data1/fog/FogNet/trained_model/single_gpu_weights.h5'
+#cnn_file_name = '/data1/fog/fognn/FogNet/trained_model/weights.h5'
+input_nam_shape       = Input((32, 32, 288, 1))
+input_mur_shape       = Input((384, 384, 1, 1)) 
+input_nam_G1_24_shape = Input((32, 32, 108, 1))
+input_nam_G2_24_shape = Input((32, 32, 96, 1))
+input_nam_G3_24_shape = Input((32, 32, 108, 1))
+input_nam_G4_24_shape = Input((32, 32, 60, 1))
+input_mixed_24_shape  = Input((32, 32, 12, 1))
 
 
+C  = FogNet.FogNet(input_nam_G1_24_shape, 
+               input_nam_G2_24_shape, 
+               input_nam_G3_24_shape, 
+               input_nam_G4_24_shape, 
+               input_mixed_24_shape, 
+               input_mur_shape, filters, dropout, 2)
 
-PFI_channels = xai_engine.PFI_channels(cnn_model_object, testing_list, Testing_targets, n_repeats=5, random_state=42)
-PFI_channels.to_csv('./PFI5.csv')
+cnn_model_object = C.BuildModel()
+#cnn_model_object = multi_gpu_model(cnn_model_object, gpus=4)
+#model.summary() 
 
-PFI_groups = xai_engine.FIGW(cnn_model_object, testing_list, Testing_targets, xai_method = 'PFI', num_iter = 5, random_state=42)
-PFI_groups.to_csv('./PFIG5.csv')
+cnn_model_object.load_weights(cnn_file_name)  
+
+cnn_model_object.compile(optimizer=Adam(lr=learningRate, decay=wd),
+      loss='categorical_crossentropy',
+      metrics=['accuracy'])
+
+
+#PFI_channels = xai_engine.PFI_channels(cnn_model_object, testing_list, Testing_targets, n_repeats=5, random_state=42)
+#PFI_channels.to_csv('./PFICW.csv')
+
+
+#PFI_groups = xai_engine.FIGW(cnn_model_object, testing_list, Testing_targets, xai_method = 'PFI', num_iter = 5, random_state=42)
+#PFI_groups.to_csv('./PFIG3.csv')
 
 LossSHAP_groups = xai_engine.FIGW(cnn_model_object, testing_list, Testing_targets, xai_method = 'LossSHAP', num_iter = 5, random_state=42)
 LossSHAP_groups.to_csv('./LossSHAP5.csv')
+
+
+
